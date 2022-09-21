38c6580914c60d42e81467985592bf2b70f84970
bruholanda <brunaholandacunha7@gmail.com>
2022-09-21 19:40:29 -0300

    Events

--- olamundo/src/App.js
+++ olamundo/src/App.js
@@ -1,13 +1,11 @@
 import TituloPrincipal from './components/TituloPrincipal';
-import OutroTitulo from './components/OutroTitulo';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TituloPrincipal tituloProps = "titulo 1" />
-      <OutroTitulo tituloProps = "titulo 2" />
+      <TituloPrincipal tituloProps = "Click me!" />
     </div>
   );
 }
--- olamundo/src/components/TituloPrincipal.jsx
+++ olamundo/src/components/TituloPrincipal.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 export default function TituloPrincipal({ tituloProps }) {
   
   const titulo = tituloProps ?? "Olá, mundo!";
+  
+  const clickCallBack = (event) => {
+    alert("Thanks for clicking!");
+  }
 
-  return <h1>{titulo}</h1>; 
+  return <h1 onClick={ clickCallBack }>{titulo}</h1>; 
 }
