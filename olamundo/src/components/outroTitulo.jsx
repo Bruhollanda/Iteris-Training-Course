840e03914e841bdff73c708e02d1560792075c49
bruholanda <brunaholandacunha7@gmail.com>
2022-09-21 18:58:46 -0300

    Componente com classe

---
+++ olamundo/src/components/outroTitulo.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class outroTitulo extends Component {
+  render() {
+    const titulo = "Olá, componente com classe!";
+    return (
+      <h1>{ titulo }</h1>
+    )
+  }
+}
