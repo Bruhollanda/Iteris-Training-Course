5c0b3bb717d8c63e8128b541b7c2abeb95a80454
bruholanda <brunaholandacunha7@gmail.com>
2022-09-21 19:18:43 -0300

    Chamando jsx com classe na App; Editado CSS da App

--- olamundo/src/App.js
+++ olamundo/src/App.js
@@ -1,8 +1,13 @@
 import TituloPrincipal from './components/TituloPrincipal';
+import OutroTitulo from './components/OutroTitulo';
+
+import './App.css';
+
 function App() {
   return (
     <div className="App">
       <TituloPrincipal />
+      <OutroTitulo />
     </div>
   );
 }
